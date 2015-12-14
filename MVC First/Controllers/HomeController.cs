b2d79cd4a52b0040be9d1b850bcc1fa1e11d8b8a
vijay narasimham <pvl.narasimham@gmail.com>
2015-12-14 16:16:26 -0600

    enabled trace and adding parameters to index

--- MVC First/Controllers/HomeController.cs
+++ MVC First/Controllers/HomeController.cs
@@ -9,9 +9,9 @@ namespace MVC_First.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public string Index()
+        public string Index(string ID, string name)
         {
-            return "Hello World... This is MVC application";
+            return "Hello World... This is MVC application " + "ID:" + ID + " Name:" + name;
         }
 
         public string Version()
